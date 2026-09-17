a6f3f23c998ec2c4946b04b576cdf08fce8840a8
gtironi-blip <g.tironi@escola.pr.gov.br>
2026-09-17 09:40:12 -0300

    3

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function botao() {
     alert ("Kwaii. UwU :)")
 }
+
+let nota1tri;
+let nota2tri;
+let resultadonota;
+
+function Calcularnota() {
+    nota1tri = Number(prompt("Digite sua nota do 1º Trimestre"));
+    nota2tri = Number(prompt("Digite sua nota do 2º Trimestre"));
+
+    resultadonota = 180 - nota1tri + nota2tri;
+    Number(alert (resultadonota));
+}
